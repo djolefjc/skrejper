ce46fb07bc90192732ee1eef83d47c5c8947be38
djolefjc <djolefjc@gmail.com>
2025-02-10 01:22:58 +0100

    Remove comments

--- src/Scraper.php
+++ src/Scraper.php
@@ -4,127 +4,104 @@ namespace Koren\Skrejper;
 
 use simplehtmldom\HtmlWeb;
 
-class Scraper {
-
-const MAIN_SHOP_PAGE_URL = "http://www.e-tiba.rs/product-category/caj-kafa-topla-cokolada/";
-const ELEMENT_CLASS = ".product-image .woocommerce-LoopProduct-link";
-const NUMBER_OF_PAGES = "";
-const SHOP_PAGE_URL = "http://www.e-tiba.rs/product-category/caj-kafa-topla-cokolada/page/";
-
-public function init($option)
+class Scraper
 {
-  if ($option == "0") {
-    $this->uno();
+
+  const MAIN_SHOP_PAGE_URL = "http://www.e-tiba.rs/product-category/caj-kafa-topla-cokolada/";
+  const ELEMENT_CLASS = ".product-image .woocommerce-LoopProduct-link";
+  const NUMBER_OF_PAGES = "";
+  const SHOP_PAGE_URL = "http://www.e-tiba.rs/product-category/caj-kafa-topla-cokolada/page/";
+
+  public function init($option)
+  {
+    if ($option == "0") {
+      $this->uno();
+    }
+    if ($option == "1") {
+      $this->uno();
+    }
+    if ($option == "2") {
+      $this->dos();
+    }
   }
-  if ($option == "1") {
-    $this->uno();
-  }
-  if ($option == "2") {
-    $this->dos();
-  } 
-}
 
-private function uno()
-{
-  $linkovi = [];
-  //Inicjializujemo simple html dom i dajemo mu response sa curl-a.
-  $client = new HtmlWeb();
-  $html = $client->load($this::MAIN_SHOP_PAGE_URL);
+  private function uno()
+  {
+    $linkovi = [];
 
-  //Tražimo elemente sa određenom klasom koju smo pronašli puten inspecta.
+    $client = new HtmlWeb();
+    $html = $client->load($this::MAIN_SHOP_PAGE_URL);
 
-  $products = $html->find($this::ELEMENT_CLASS);
+    $products = $html->find($this::ELEMENT_CLASS);
 
-  //Lupujemo kroz elemente i stavljamo linkove u niz
-  foreach($products as $p){
-  $linkovi[] = $p->href;
-  }
-  if ($this::NUMBER_OF_PAGES > 1) {
-    for($i = 1; $i <= $this::NUMBER_OF_PAGES; $i++){
+    foreach ($products as $p) {
+      $linkovi[] = $p->href;
+    }
 
-      //Inicjializujemo simple html dom i dajemo mu response sa curl-a.
-      $client = new HtmlWeb();
-      $html = $client->load($this::SHOP_PAGE_URL . $i);
-      
-      //Tražimo elemente sa određenom klasom.
-      $products = $html->find($this::ELEMENT_CLASS);
-      
-      //Lupujemo kroz elemente i stavljamo linkove u niz
-      foreach($products as $p){
-        $linkovi[] = $p->href;
+    if ($this::NUMBER_OF_PAGES > 1) {
+      for ($i = 1; $i <= $this::NUMBER_OF_PAGES; $i++) {
+        $client = new HtmlWeb();
+        $html = $client->load($this::SHOP_PAGE_URL . $i);
+
+        $products = $html->find($this::ELEMENT_CLASS);
+
+        foreach ($products as $p) {
+          $linkovi[] = $p->href;
         }
       }
     }
-  //Izbacivanje duplikata.
-  $links = array_unique($linkovi);
+    $links = array_unique($linkovi);
+    $myfile = fopen("links.txt", "w") or die("Unable to open file!");
+    $txt = "";
 
-  //Kreiranje fajla i upis linkova u isti.
-  $myfile = fopen("links.txt", "w") or die("Unable to open file!");
-  $txt = "";
-    
-  foreach($links as $link){
-  $txt .= $link."\n";
+    foreach ($links as $link) {
+      $txt .= $link . "\n";
+    }
+
+    fwrite($myfile, $txt);
+    fclose($myfile);
+
+    echo "DONE\n";
   }
 
-  fwrite($myfile, $txt);
-  fclose($myfile);
+  private function dos()
+  {
+    if (filesize('links.txt') != 0) {
+      $lines = file('links.txt');
+      $i = 1;
+      $row = [];
 
- echo "DONE\n";
-}
+      foreach ($lines as $line) {
+        $client = new HtmlWeb();
+        $line = trim($line);
+        $html = $client->load($line);
 
-private function dos()
-{
-//Proverava da li je tekstualni dokument sa linkovima prazan
-if (filesize('links.txt') != 0){
+        $product_img = "";
+        $img = ".woocommerce-product-gallery__image a";
 
-//Otvara links.txt
-$lines = file('links.txt');
-//Brojac za varijablu koju kasnije koristimo za export proizvoda u csv.
-$i = 1;
-//Niz koji koristimo za export proizvoda u CSV
-$row = [];
+        foreach ($html->find($img) as $t) {
+          $product_img = $t->href;
+        }
+        $row[$i]['img'] = $product_img;
+        $i++;
+      } 
+      //EXPORT U CSV
+      $delimiter = ",";
+      $filename = "data" . ".csv"; // Create file name
+      $f = fopen('memory.txt', 'r+');
 
-foreach($lines as $line){
-  $client = new HtmlWeb();
-  $line = trim($line);
-  $html = $client->load($line);
-
-  $product_img = "";
-  //VARIJABLE ZA POPUNITI
-  $img = ".woocommerce-product-gallery__image a";
-
-  //VARIJABLE ZA POPUNITI - END
-  //Izvlacenje vrednosti sa stranice.
-  foreach($html->find($img) as $t){
-  $product_img = $t->href;
-  }
-//Dodavanje vrednosti u niz koji koristimo za export u csv.
-  $row[$i]['img'] = $product_img;
-
-  $i++;
-  } //Kraj foreach loop-a linkova
-//EXPORT U CSV
-$delimiter = ",";
-$filename = "data" . ".csv"; // Create file name
-
-// Napravi pointer
-$f = fopen('memory.txt', 'r+');
-
-//Postavi hedere za kolone tabele.
-//WOO FIELDS
-// $fields = array('featured_image','post_title','post_category','post_content','post_author','post_date','post_format','comment_status','post_status');
-$fields = ['Image'];
-//Izbaci sve podatke u redu i stavi u pointer.
-foreach($row as $r){
-   $lineData = [$r['img']];
-   fputcsv($f, $lineData, $delimiter);
- }
-//Vrati se na pocetak fajla.
-fseek($f, 0);
-
-// Piši po postavljenom pointeru.
-fpassthru($f);
-    } 
+      //WOO FIELDS
+      // $fields = array('featured_image','post_title','post_category','post_content','post_author','post_date','post_format','comment_status','post_status');
+      
+      $fields = ['Image'];
+      fputcsv($f, $fields, $delimiter);
+      foreach ($row as $r) {
+        $lineData = [$r['img']];
+        fputcsv($f, $lineData, $delimiter);
+      }
+      fseek($f, 0);
+      fpassthru($f);
+    }
   }
 
 }
